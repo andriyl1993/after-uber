286b1d690fa790043fd9b4c478a9aa49fcfe6287
andriyl1993 <andriyl1993@gmail.com>
2016-08-22 00:11:40 +0300

    fix mistake

--- after_uber/main/wrappers.py
+++ after_uber/main/wrappers.py
@@ -6,9 +6,9 @@ def address_validator(fn):
 
 	def wrapper(request):
 		data = request.POST
-		if 'start' in data and 'end' in data:
+		if data.get('start') and 'end' in data.get('end'):
 			return fn(request)
 		else:
-			return HttpResponse({'result': 'error'})
+			return HttpResponse(json.dumps({'result': 'error'}))
 
 	return wrapper
